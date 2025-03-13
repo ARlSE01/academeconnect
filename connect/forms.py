5e1e881209f8909e0b0ad50e3a933a94f2ad2184
Smrity19 <smritytripathi19@gmail.com>
2025-03-13 14:45:16 +0530

    Designed Registration Page

--- connect/forms.py
+++ connect/forms.py
@@ -2,9 +2,18 @@ from django import forms
 from .models import *
 
 class UserForm(forms.Form):
-    Username = forms.CharField(max_length=200)
-    Email = forms.EmailField(max_length=50)
-    Password = forms.CharField(widget=forms.PasswordInput())
+    # Username = forms.CharField(max_length=200)
+    # Email = forms.EmailField(max_length=50)
+    # Password = forms.CharField(widget=forms.PasswordInput())
+    Email = forms.EmailField(widget=forms.EmailInput(attrs={
+        'class': 'w-full p-3 rounded-lg bg-white/20 text-white placeholder-gray-300 shadow-md focus:ring-2 focus:ring-teal-400 outline-none'
+    }))
+    Username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'w-full p-3 rounded-lg bg-white/20 text-white placeholder-gray-300 shadow-md focus:ring-2 focus:ring-teal-400 outline-none'
+    }))
+    Password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'w-full p-3 rounded-lg bg-white/20 text-white placeholder-gray-300 shadow-md focus:ring-2 focus:ring-teal-400 outline-none'
+    }))
     # Tags = forms.MultipleChoiceField(
     #     choices=[('html', 'HTML'), ('css', 'CSS'), ('javascript', 'JavaScript')],
     #     widget=forms.CheckboxSelectMultiple,
