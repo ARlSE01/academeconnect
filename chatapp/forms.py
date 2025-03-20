f8e2517cacb04d349f91789dce40e1127b1c71e1
Soumik Ghosh <ghosh@MacBook-Pro.local>
2025-03-20 20:47:51 +0530

    chatttttt

--- chatapp/forms.py
+++ chatapp/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.forms import ModelForm
 from .models import *
+from connect.models import Tags
 
 class ChatmessageCreateForm(ModelForm):
     class Meta:
@@ -9,3 +10,12 @@ class ChatmessageCreateForm(ModelForm):
         widgets = {
             'body' : forms.TextInput(attrs={'placeholder': 'Add message ...', 'class': 'p-4 text-black','autofocus': True }),
         }
+
+class TagForm(forms.Form):
+    tags = forms.ModelChoiceField(
+        queryset=Tags.objects.all(),  # Empty initially
+        widget=forms.RadioSelect(attrs={
+            'class': 'text-white',  # Style for the checkbox list
+        }),
+        required=False
+    )
--- chatapp/models.py
+++ chatapp/models.py
@@ -2,11 +2,16 @@
 from django.db import models
 from django.db.models import CASCADE
 from connect.models import *
+import shortuuid
+
+
 
 # Create your models here.
 
 class ChatGroup(models.Model):
-    group_name = models.CharField(max_length=128, unique=True)
+    group_name = models.CharField(max_length=128, unique=True,default=shortuuid.uuid)
+    members = models.ManyToManyField(User,related_name='chat_groups',blank=True)
+    is_private = models.BooleanField(default=False)
 
     def __str__(self):
         return self.group_name
