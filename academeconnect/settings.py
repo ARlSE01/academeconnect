dd52553b3bc59d5850f8743e8b99b590a394c951
ARlSE01 <143578068+ARlSE01@users.noreply.github.com>
2025-03-31 21:10:12 +0530

    Update settings.py

--- academeconnect/settings.py
+++ academeconnect/settings.py
@@ -33,7 +33,7 @@ INTERNAL_IPS = [
 ]
 TAILWIND_APP_NAME = 'theme'
 NPM_BIN_PATH = r"/Users/ghosh/.nvm/versions/node/v22.14.0/bin/npm"
-CSRF_TRUSTED_ORIGINS = ['https://*.mydomain.com','https://*.127.0.0.1','https://d3fd-103-218-237-74.ngrok-free.app']
+CSRF_TRUSTED_ORIGINS = ['*']
 
 
 # Application definition
