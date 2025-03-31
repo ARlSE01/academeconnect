324c586d7017f7f6c287c7f8bff36aa2dfa426ad
Soumik Ghosh <ghosh@MacBook-Pro.local>
2025-03-31 11:17:16 +0530

    login updates

--- connect/urls.py
+++ connect/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('',views.homepage,name='homepage'),
-    path('registration',views.registration),
+    path('registration',views.registration,name='registration'),
     path('createpost',views.createpost, name="createpost"),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
